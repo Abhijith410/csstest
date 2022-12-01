7cd0d92577515d78d4e945e56cd92b780853b484
Abhijith <ayabhijith@gmail.com>
2022-12-01 15:11:22 +0530

    csstest

--- orkut/urls.py
+++ orkut/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('app/', views.fun1, name="media.html")
+    path('app/', views.fun1, name="media"),
+    path('messages/', views.fun2, name="message"),
+    path('profile/', views.fun3, name="profile"),
 ]
--- orkut/views.py
+++ orkut/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def fun1(request):
     return render (request,"media.html",)
+
+def fun2(request):
+    return render (request, "messages.html",)
+
+def fun3(request):
+    return render (request, "profile.html",)        
